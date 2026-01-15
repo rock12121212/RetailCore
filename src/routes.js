49f978f8c018eb1b6299b35903278194a2b9fdfe
dushyant <dushyant.singh@techstalwarts.com>
2026-01-15 16:42:59 +0530

    add

--- src/routes.js
+++ src/routes.js
@@ -13,6 +13,7 @@ import subscriptionRoutes from './modules/subscription/subscription.routes.js';
 const router = Router();
 
 // Register module routes
+// add route
 router.use('/auth', authRoutes);
 router.use('/users', userRoutes);
 router.use('/videos', videoRoutes);
