0ab7fdf587abcab76b988f18ebd6b0771abe9b1c
Darshan Jamgade <darshan.jamgade@techstalwarts.com>
2026-01-16 18:00:02 +0530

    updated verify jwt

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -2,6 +2,7 @@ import jwt from 'jsonwebtoken';
 import { ApiError } from '../utils/apiError.js';
 import { asyncHandler } from '../utils/asyncHandler.js';
 import { env } from '../config/env.config.js';
+import { User } from '../modules/user/user.model.js';
 
 export const verifyJWT = asyncHandler(async (req, _res, next) => {
   try {
@@ -12,13 +13,16 @@ export const verifyJWT = asyncHandler(async (req, _res, next) => {
     }
 
     const decodedToken = jwt.verify(token, env.jwt.secret);
-    
-    // In a real app, you'd fetch the user from DB here
-    // const user = await User.findById(decodedToken?._id).select('-password');
-    // if (!user) throw new ApiError(401, 'Invalid Access Token');
-    // req.user = user;
 
-    req.user = decodedToken;
+    const user = await User.findById(decodedToken?._id).select(
+      '-password -refreshToken'
+    );
+
+    if (!user) {
+      throw new ApiError(401, 'Invalid Access Token');
+    }
+
+    req.user = user;
     next();
   } catch (error) {
     throw new ApiError(401, error?.message || 'Invalid access token');
